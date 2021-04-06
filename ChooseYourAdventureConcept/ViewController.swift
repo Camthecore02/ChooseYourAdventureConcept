b611d741f6b41291c1a2b122e07e14c8cd325a20
James Jermann <jamesjermann@gmail.com>
2021-04-06 14:36:07 -0400

    Errors Resolved 04/06

--- ChooseYourAdventureConcept/ViewController.swift
+++ ChooseYourAdventureConcept/ViewController.swift
@@ -23,45 +23,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let samplePage = NewPage(pageImage: "maskedCat", pageText: "Meow",  buttonTitles: ["yes", "no"], buttonLinks: [yesPage, noPage])
+       //  let samplePage = NewPage(pageImage: "maskedCat", pageText: "Meow",  buttonTitles: ["yes", "no"], buttonLinks: [yesPage, noPage])
+        
 //            let yesPage = NewPage(pageImage: "maskedCat", pageText: "Yes Page", buttonTitles: ["Back to Sample"], buttonLinks: [samplePage])
 //        let noPage = NewPage(pageImage: "maskedCat", pageText: "No Page", buttonTitles: ["Back to Sample"], buttonLinks: samplePage)
         
         
         
-        loadPage(whatPage: samplePage)
+        loadPage(whatPage: yesPage)
+        
     }
-    func loadPage(whatPage: String){
-        currentPage
-        pageImage.image = UIImage(named: whatPage.pageImage)
-        pageText.text = whatPage.pageText
+    func loadPage(whatPage: NewPage){ //NewPage
+        
+        currentPage = whatPage
+        pageImage.image = UIImage(named: currentPage.pageImage)
+        pageText.text = currentPage.pageText
        
        //switch case if else if. if array.count hide all but 2, then update array
-        if whatPage.buttonTitles.count == 1{
+        if currentPage.buttonTitles.count == 1{
             buttonTwo.titleLabel?.text = ""
             buttonThree.titleLabel?.text = ""
             ButtonFour.titleLabel?.text = ""
-            buttonOne.titleLabel?.text = whatPage.buttonTitles[0]
+            buttonOne.titleLabel?.text = currentPage.buttonTitles[0]
             
         } else if whatPage.buttonTitles.count == 2{
            // buttonTwo.isHidden = true
             buttonThree.titleLabel?.text = ""
             ButtonFour.titleLabel?.text = ""
-            buttonOne.setTitle(whatPage.buttonTitles[0], for: .normal)
-            buttonTwo.setTitle(whatPage.buttonTitles[1], for: .normal)
+            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
+            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
             //button.setTitle("Button Title", for: .normal)
-        } else if whatPage.buttonTitles.count == 3{
+        } else if currentPage.buttonTitles.count == 3{
             // buttonTwo.isHidden = true
-            buttonThree.setTitle(whatPage.buttonTitles[2], for: .normal)
+            buttonThree.setTitle(currentPage.buttonTitles[2], for: .normal)
             ButtonFour.titleLabel?.text = ""
-            buttonOne.setTitle(whatPage.buttonTitles[0], for: .normal)
-            buttonTwo.setTitle(whatPage.buttonTitles[1], for: .normal)
+            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
+            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
         } else if whatPage.buttonTitles.count == 4{
             // buttonTwo.isHidden = true
-            buttonThree.setTitle(whatPage.buttonTitles[2], for: .normal)
-            ButtonFour.setTitle(whatPage.buttonTitles[3], for: .normal)
-            buttonOne.setTitle(whatPage.buttonTitles[0], for: .normal)
-            buttonTwo.setTitle(whatPage.buttonTitles[1], for: .normal)
+            buttonThree.setTitle(currentPage.buttonTitles[2], for: .normal)
+            ButtonFour.setTitle(currentPage.buttonTitles[3], for: .normal)
+            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
+            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
         } else {
             buttonTwo.titleLabel?.text = ""
             buttonThree.titleLabel?.text = ""
