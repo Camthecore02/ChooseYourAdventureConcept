db91672d47545960902ed12d98f0da79c52f2499
James Jermann <jamesjermann@gmail.com>
2021-04-21 12:48:05 -0400

    removed un needed clutter before working

--- ChooseYourAdventureConcept/ViewController.swift
+++ ChooseYourAdventureConcept/ViewController.swift
@@ -20,8 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ButtonFour: UIButton!
     
    // var currentPage: NewPage = samplePage
-    
-    var text: String = ""
+   
    
     
     override func viewDidLoad() {
@@ -30,68 +29,7 @@ class ViewController: UIViewController {
       
         
         
-//       //  let samplePage = NewPage(pageImage: "maskedCat", pageText: "Meow",  buttonTitles: ["yes", "no"], buttonLinks: [yesPage, noPage])
-//
-////            let yesPage = NewPage(pageImage: "maskedCat", pageText: "Yes Page", buttonTitles: ["Back to Sample"], buttonLinks: [samplePage])
-////        let noPage = NewPage(pageImage: "maskedCat", pageText: "No Page", buttonTitles: ["Back to Sample"], buttonLinks: samplePage)
-//
-//
-//
-//        loadPage(whatPage: yesPage)
-//
-//    }
-//    func loadPage(whatPage: NewPage){ //NewPage
-//
-//        currentPage = whatPage
-//        pageImage.image = UIImage(named: currentPage.pageImage)
-//        pageText.text = currentPage.pageText
-//
-//       //switch case if else if. if array.count hide all but 2, then update array
-//        if currentPage.buttonTitles.count == 1{
-//            buttonTwo.titleLabel?.text = ""
-//            buttonThree.titleLabel?.text = ""
-//            ButtonFour.titleLabel?.text = ""
-//            buttonOne.titleLabel?.text = currentPage.buttonTitles[0]
-//
-//        } else if whatPage.buttonTitles.count == 2{
-//           // buttonTwo.isHidden = true
-//            buttonThree.titleLabel?.text = ""
-//            ButtonFour.titleLabel?.text = ""
-//            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
-//            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
-//            //button.setTitle("Button Title", for: .normal)
-//        } else if currentPage.buttonTitles.count == 3{
-//            // buttonTwo.isHidden = true
-//            buttonThree.setTitle(currentPage.buttonTitles[2], for: .normal)
-//            ButtonFour.titleLabel?.text = ""
-//            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
-//            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
-//        } else if whatPage.buttonTitles.count == 4{
-//            // buttonTwo.isHidden = true
-//            buttonThree.setTitle(currentPage.buttonTitles[2], for: .normal)
-//            ButtonFour.setTitle(currentPage.buttonTitles[3], for: .normal)
-//            buttonOne.setTitle(currentPage.buttonTitles[0], for: .normal)
-//            buttonTwo.setTitle(currentPage.buttonTitles[1], for: .normal)
-//        } else {
-//            buttonTwo.titleLabel?.text = ""
-//            buttonThree.titleLabel?.text = ""
-//            ButtonFour.titleLabel?.text = ""
-//            buttonOne.titleLabel?.text = ""
-//        }
-//
-//}
-//
-//    @IBAction func answerTapped(_ sender: UIButton) {
-////        switch sender {
-////        case 0:
-////            <#code#>
-////        default:
-////            loadPage (whatPage: currentPage.buttonLinks[sender.titleLabel!.text])
-////        }
-//        print(sender.titleLabel?.text)
-//    }
-//}
-//// Next step: Find out how to change button titles based on previous options. Possible fix: Use the new page file to write multiple stories based on previous buttons.
+
         
 }
     @IBAction func choice1(_ sender: UIButton) {
@@ -99,7 +37,7 @@ class ViewController: UIViewController {
         option2 = false
         option3 = false
           desision()
-        performSegue(withIdentifier: "segueToSecond", sender: nil)
+        
       
     }
     
